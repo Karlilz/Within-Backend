69c5221afe54290a3c62a751386543146e35db94
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-11-16 15:07:15 -0500

    updated code to deploy

--- model/Goal.js
+++ model/Goal.js
@@ -2,11 +2,11 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const goalSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User', 
-    required: true,
-  },
+  // user: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'User', 
+  //   required: true,
+  // },
   content: {
     type: String,
     required: true,
@@ -15,19 +15,19 @@ const goalSchema = new mongoose.Schema({
     type: Date,
     required: true,
   },
-  completionProgress: {
-    type: Number,
-    default: 0,
-    max: 100,
-  },
-  completed: {
-    type: Boolean,
-    default: false, 
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
+  // completionProgress: {
+  //   type: Number,
+  //   default: 0,
+  //   max: 100,
+  // },
+  // completed: {
+  //   type: Boolean,
+  //   default: false, 
+  // },
+  // createdAt: {
+  //   type: Date,
+  //   default: Date.now,
+  // },
 });
 
 const Goal = mongoose.model('Goal', goalSchema);
