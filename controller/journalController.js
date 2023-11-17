8bc7876dea3e9183c73eca2ea5963f80be884513
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-11-17 18:08:34 -0500

    journal controller fixed

--- controller/journalController.js
+++ controller/journalController.js
@@ -1,3 +1,123 @@
+// // const express = require('express');
+// // const router = express.Router();
+// // const JournalEntry = require('../model/JournalEntry');
+
+// // // INDEX ROUTE 
+// // router.get('/', async (req, res) => {
+// //   try {
+// //     // Assuming you have implemented user authentication middleware
+// //     const userId = req.user._id;
+
+// //     // Retrieve mood journal entries for the specific user
+// //     const entries = await JournalEntry.find({ user: userId }).sort({ date: 'desc' });
+
+// //     res.status(200).json(entries);
+// //   } catch (error) {
+// //     console.error('Error fetching journal entries:', error);
+// //     res.status(500).json({ error: 'Internal Server Error' });
+// //   }
+// // });
+
+// // // NEW ROUTE
+
+// // // DELETE ROUTE 
+// // router.delete('/:id', async (req, res) => {
+// //   try {
+// //     // Assuming you have implemented user authentication middleware
+// //     const userId = req.user._id;
+
+// //     // Delete the mood journal entry for the user
+// //     const deletedEntry = await JournalEntry.findOneAndDelete({ _id: req.params.id, user: userId });
+
+// //     if (!deletedEntry) {
+// //       return res.status(404).json({ error: 'Entry not found' });
+// //     }
+
+// //     res.status(200).json({ message: 'Journal entry deleted successfully', entry: deletedEntry });
+// //   } catch (error) {
+// //     console.error('Error deleting journal entry:', error);
+// //     res.status(500).json({ error: 'Internal Server Error' });
+// //   }
+// // });
+
+// // // UPDATE ROUTE 
+// // router.put('/:id', async (req, res) => {
+// //   try {
+// //     // Assuming you have implemented user authentication middleware
+// //     const userId = req.user._id;
+
+// //     const { content } = req.body;
+
+// //     // Update the mood journal entry for the user
+// //     const updatedEntry = await JournalEntry.findOneAndUpdate(
+// //       { _id: req.params.id, user: userId },
+// //       { content },
+// //       { new: true } // Return the updated entry
+// //     );
+
+// //     if (!updatedEntry) {
+// //       return res.status(404).json({ error: 'Entry not found' });
+// //     }
+
+// //     res.status(200).json({ message: 'Journal entry updated successfully', entry: updatedEntry });
+// //   } catch (error) {
+// //     console.error('Error updating journal entry:', error);
+// //     res.status(500).json({ error: 'Internal Server Error' });
+// //   }
+// // });
+
+// // // CREATE ROUTE 
+// // router.post('/', async (req, res) => {
+// //   try {
+// //     // Assuming you have implemented user authentication middleware
+// //     console.log(req.user)
+// //     const userId = req.user._id;
+
+// //     const { content } = req.body;
+
+// //     // Create a new mood journal entry
+// //     const newEntry = new JournalEntry({
+// //       content,
+// //       user: userId,
+// //       date: new Date(), // You can customize the date format based on your preference
+// //     });
+
+// //     // Save the entry to the database
+// //     await newEntry.save();
+
+// //     res.status(201).json({ message: 'Journal entry added successfully', entry: newEntry });
+// //   } catch (error) {
+// //     console.error('Error adding journal entry:', error);
+// //     res.status(500).json({ error: 'Internal Server Error' });
+// //   }
+// // });
+
+// // // EDIT ROUTE
+
+// // // SHOW ROUTE
+// // router.get('/:id', async (req, res) => {
+// //   try {
+// //     // Assuming you have implemented user authentication middleware
+// //     const userId = req.user._id;
+
+// //     // Retrieve the specific mood journal entry for the user
+// //     const entry = await JournalEntry.findOne({ _id: req.params.id, user: userId });
+
+// //     if (!entry) {
+// //       return res.status(404).json({ error: 'Entry not found' });
+// //     }
+
+// //     res.status(200).json(entry);
+// //   } catch (error) {
+// //     console.error('Error fetching journal entry:', error);
+// //     res.status(500).json({ error: 'Internal Server Error' });
+// //   }
+// // });
+
+// // module.exports = router;
+
+
+// // WITHOUT AUTEHNTICATION MIDDLEWARE
 // const express = require('express');
 // const router = express.Router();
 // const JournalEntry = require('../model/JournalEntry');
@@ -5,11 +125,12 @@
 // // INDEX ROUTE 
 // router.get('/', async (req, res) => {
 //   try {
-//     // Assuming you have implemented user authentication middleware
-//     const userId = req.user._id;
+//     // // Modify this part according to your authentication or authorization mechanism
+//     // // For example, you might extract user ID from a token, session, or request headers
+//     // const userId = getUserIdFromRequest(req);
 
-//     // Retrieve mood journal entries for the specific user
-//     const entries = await JournalEntry.find({ user: userId }).sort({ date: 'desc' });
+//     // // Retrieve mood journal entries for the specific user
+//     // const entries = await JournalEntry.find({ user: userId }).sort({ date: 'desc' });
 
 //     res.status(200).json(entries);
 //   } catch (error) {
@@ -18,15 +139,12 @@
 //   }
 // });
 
-// // NEW ROUTE
-
 // // DELETE ROUTE 
 // router.delete('/:id', async (req, res) => {
 //   try {
-//     // Assuming you have implemented user authentication middleware
-//     const userId = req.user._id;
+//     // Modify this part according to your authentication or authorization mechanism
+//     const userId = getUserIdFromRequest(req);
 
-//     // Delete the mood journal entry for the user
 //     const deletedEntry = await JournalEntry.findOneAndDelete({ _id: req.params.id, user: userId });
 
 //     if (!deletedEntry) {
@@ -43,12 +161,11 @@
 // // UPDATE ROUTE 
 // router.put('/:id', async (req, res) => {
 //   try {
-//     // Assuming you have implemented user authentication middleware
-//     const userId = req.user._id;
+//     // Modify this part according to your authentication or authorization mechanism
+//     const userId = getUserIdFromRequest(req);
 
 //     const { content } = req.body;
 
-//     // Update the mood journal entry for the user
 //     const updatedEntry = await JournalEntry.findOneAndUpdate(
 //       { _id: req.params.id, user: userId },
 //       { content },
@@ -67,38 +184,47 @@
 // });
 
 // // CREATE ROUTE 
-// router.post('/', async (req, res) => {
-//   try {
-//     // Assuming you have implemented user authentication middleware
-//     console.log(req.user)
-//     const userId = req.user._id;
+// // router.post('/', async (req, res) => {
+// //   try {
+// //     // Modify this part according to your authentication or authorization mechanism
+// //     const userId = getUserIdFromRequest(req);
 
-//     const { content } = req.body;
+// //     const { content } = req.body;
 
-//     // Create a new mood journal entry
-//     const newEntry = new JournalEntry({
-//       content,
-//       user: userId,
-//       date: new Date(), // You can customize the date format based on your preference
-//     });
+// //     const newEntry = new JournalEntry({
+// //       content,
+// //       user: userId,
+// //       date: new Date(), 
+// //     });
 
-//     // Save the entry to the database
-//     await newEntry.save();
+// //     await newEntry.save();
 
-//     res.status(201).json({ message: 'Journal entry added successfully', entry: newEntry });
-//   } catch (error) {
-//     console.error('Error adding journal entry:', error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
+// //     res.status(201).json({ message: 'Journal entry added successfully', entry: newEntry });
+// //   } catch (error) {
+// //     console.error('Error adding journal entry:', error);
+// //     res.status(500).json({ error: 'Internal Server Error' });
+// //   }
+// // });
+
+
+// // CREATE ROUTE
+// router.post("/", async (req,res) => {
+//   console.log(req.body)
+//     try {
+//       let response = await Journal.create(req.body)
+//       console.log(response)
+//         res.json(response)
+//     }catch (error){
+//         res.status(400).json(error)
+//     }
 // });
 
-// // EDIT ROUTE
 
 // // SHOW ROUTE
 // router.get('/:id', async (req, res) => {
 //   try {
-//     // Assuming you have implemented user authentication middleware
-//     const userId = req.user._id;
+//     // Modify this part according to your authentication or authorization mechanism
+//     const userId = getUserIdFromRequest(req);
 
 //     // Retrieve the specific mood journal entry for the user
 //     const entry = await JournalEntry.findOne({ _id: req.params.id, user: userId });
@@ -114,127 +240,95 @@
 //   }
 // });
 
+// // You need to implement a function that extracts user ID from the request
+// function getUserIdFromRequest(req) {
+//   // Implement your logic here to get the user ID from the request
+//   // For example, you might extract it from a token, session, or request headers
+//   // Return the user ID
+// }
+  
 // module.exports = router;
 
 
-// WITHOUT AUTEHNTICATION MIDDLEWARE
+
 const express = require('express');
 const router = express.Router();
 const JournalEntry = require('../model/JournalEntry');
 
-// INDEX ROUTE 
-router.get('/', async (req, res) => {
+// INDEX ROUTE
+router.get("/", async (req, res) => {
   try {
-    // Modify this part according to your authentication or authorization mechanism
-    // For example, you might extract user ID from a token, session, or request headers
-    const userId = getUserIdFromRequest(req);
-
-    // Retrieve mood journal entries for the specific user
-    const entries = await JournalEntry.find({ user: userId }).sort({ date: 'desc' });
-
-    res.status(200).json(entries);
+    const userId = req.user.id;
+    const entries = await JournalEntry.find({ user: userId });
+    res.json(entries);
   } catch (error) {
-    console.error('Error fetching journal entries:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(400).json(error);
   }
 });
 
-// DELETE ROUTE 
-router.delete('/:id', async (req, res) => {
+// DELETE ROUTE
+router.delete("/:id", async (req, res) => {
   try {
-    // Modify this part according to your authentication or authorization mechanism
-    const userId = getUserIdFromRequest(req);
-
-    // Delete the mood journal entry for the user
-    const deletedEntry = await JournalEntry.findOneAndDelete({ _id: req.params.id, user: userId });
-
-    if (!deletedEntry) {
-      return res.status(404).json({ error: 'Entry not found' });
-    }
-
-    res.status(200).json({ message: 'Journal entry deleted successfully', entry: deletedEntry });
+    // const userId = req.user.id;
+    // const entry = await JournalEntry.findOneAndDelete({ _id: req.params.id, user: userId });
+    const entry = await JournalEntry.findOneAndDelete({ _id: req.params.id});
+    res.json(entry);
   } catch (error) {
-    console.error('Error deleting journal entry:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.log(error)
+    res.status(400).json(error);
   }
 });
 
 // UPDATE ROUTE 
 router.put('/:id', async (req, res) => {
   try {
-    // Modify this part according to your authentication or authorization mechanism
-    const userId = getUserIdFromRequest(req);
+    // const userId = req.user.id;
+    // const entry = await JournalEntry.findOne({ _id: req.params.id, user: userId });
+    const entry = await JournalEntry.findOne({ _id: req.params.id});
 
-    const { content } = req.body;
 
-    // Update the mood journal entry for the user
-    const updatedEntry = await JournalEntry.findOneAndUpdate(
-      { _id: req.params.id, user: userId },
-      { content },
-      { new: true } // Return the updated entry
-    );
-
-    if (!updatedEntry) {
-      return res.status(404).json({ error: 'Entry not found' });
+    if (!entry) {
+      return res.status(404).json({ message: 'Journal entry not found' });
     }
 
-    res.status(200).json({ message: 'Journal entry updated successfully', entry: updatedEntry });
+    entry.title = req.body.title;
+    entry.content = req.body.content;
+    entry.date = req.body.date;
+
+    await entry.save();
+
+    res.status(200).json(entry);
   } catch (error) {
-    console.error('Error updating journal entry:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error(error);
+    res.status(400).json(error);
   }
 });
 
-// CREATE ROUTE 
-router.post('/', async (req, res) => {
+// CREATE ROUTE
+router.post("/", async (req, res) => {
   try {
-    // Modify this part according to your authentication or authorization mechanism
-    const userId = getUserIdFromRequest(req);
+    // const userId = req.user.id;
 
-    const { content } = req.body;
+    // req.body.user = userId;
 
-    // Create a new mood journal entry
-    const newEntry = new JournalEntry({
-      content,
-      user: userId,
-      date: new Date(), // You can customize the date format based on your preference
-    });
-
-    // Save the entry to the database
-    await newEntry.save();
-
-    res.status(201).json({ message: 'Journal entry added successfully', entry: newEntry });
+    let response = await JournalEntry.create(req.body);
+    res.json(response);
   } catch (error) {
-    console.error('Error adding journal entry:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.log(error)
+    res.status(400).json(error);
+
   }
 });
 
 // SHOW ROUTE
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
-    // Modify this part according to your authentication or authorization mechanism
-    const userId = getUserIdFromRequest(req);
-
-    // Retrieve the specific mood journal entry for the user
+    const userId = req.user.id;
     const entry = await JournalEntry.findOne({ _id: req.params.id, user: userId });
-
-    if (!entry) {
-      return res.status(404).json({ error: 'Entry not found' });
-    }
-
-    res.status(200).json(entry);
+    res.json(entry);
   } catch (error) {
-    console.error('Error fetching journal entry:', error);
-    res.status(500).json({ error: 'Internal Server Error' });
+    res.status(400).json(error);
   }
 });
 
-// You need to implement a function that extracts user ID from the request
-function getUserIdFromRequest(req) {
-  // Implement your logic here to get the user ID from the request
-  // For example, you might extract it from a token, session, or request headers
-  // Return the user ID
-}
-  
 module.exports = router;
